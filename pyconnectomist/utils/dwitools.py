d2a778f68c1f11bd4cf8da90a4fd35562f5f9279
Antoine Grigis <antoine.grigis@cea.fr>
2016-10-24 10:33:08 +0200

    pyconnectomist/utils/dwitools: add function to extract shell + deal with small bvals.

--- pyconnectomist/utils/dwitools.py
+++ pyconnectomist/utils/dwitools.py
@@ -16,6 +16,89 @@ import numpy
 import math
 import os
 import numpy as np
+import nibabel
+
+
+def extract_dwi_shells(dwi_nii_path, bvals_path, bvecs_path, outdir):
+    """ Convert a multi-shell serie to multiple single shell series.
+
+    Parameters
+    ----------
+    dwi_nii_path: str
+        path to the diffusion volume file.
+    bvals_path: str
+        path to the diffusion b-values file.
+    bvecs_path: str
+        path to the diffusion b-vectors file.
+    outdir: str
+        path to the destination folder.
+
+    Returns
+    -------
+    nodiff_file: str
+        path to the mean b0 file.
+    dwis: dict
+        path the each shell 'dwi', 'bvals' and 'bvecs' files: b-values are
+        the keys of this dictionary.
+    """
+    # Load input data
+    dwi = nibabel.load(dwi_nii_path)
+    dwi_array = dwi.get_data()
+    real_bvals, real_bvecs, nb_shells, nb_nodiff = read_bvals_bvecs(
+        bvals_path, bvecs_path, min_bval=100.)
+
+    # Detect shell indices
+    bvals = real_bvals.copy()
+    bvecs = real_bvecs.copy()
+    b0_indices = numpy.where(bvals < 50)[0].tolist()
+    bvals[b0_indices] = 0
+    bvals = [int(round(bval, -2)) for bval in bvals]
+    bvals_set = set(bvals) - {0}
+
+    # Create mean b0 image
+    b0 = numpy.mean(dwi_array[..., b0_indices], axis=3)
+    im = nibabel.Nifti1Image(b0, affine=dwi.get_affine())
+    nodiff_file = os.path.join(outdir, "nodiff.nii.gz")
+    nibabel.save(im, nodiff_file)
+    b0.shape += (1, )
+
+    # Create dwi for each shell
+    dwis = {}
+    bvals = numpy.asarray(bvals)
+    for bval in bvals_set:
+
+        # Get shell indices
+        bval_outdir = os.path.join(outdir, str(bval))
+        if not os.path.isdir(bval_outdir):
+            os.mkdir(bval_outdir)
+        shell_indices = numpy.where(bvals == bval)[0].tolist()
+
+        # Create single shell dwi
+        shell_dwi = dwi_array[..., shell_indices]
+        shell_dwi = numpy.concatenate((b0, shell_dwi), axis=3)
+        im = nibabel.Nifti1Image(shell_dwi, affine=dwi.get_affine())
+        dwi_file = os.path.join(bval_outdir, "dwi.nii.gz")
+        nibabel.save(im, dwi_file)
+
+        # Create associated bvecs/bvals
+        shell_bvecs = real_bvecs[shell_indices]
+        shell_bvecs = numpy.concatenate((numpy.zeros((1, 3)), shell_bvecs),
+                                        axis=0)
+        bvecs_file = os.path.join(bval_outdir, "bvecs")
+        numpy.savetxt(bvecs_file, shell_bvecs)
+        shell_bvals = real_bvals[shell_indices]
+        shell_bvals = numpy.concatenate((numpy.zeros((1, )), shell_bvals))
+        bvals_file = os.path.join(bval_outdir, "bvals")
+        numpy.savetxt(bvals_file, shell_bvals)
+
+        # Update output structure
+        dwis[bval] = {
+            "bvals": bvals_file,
+            "bvecs": bvecs_file,
+            "dwi": dwi_file
+        }
+
+    return nodiff_file, dwis
 
 
 def read_bvals_bvecs(bvals_path, bvecs_path, min_bval=100.):
@@ -79,8 +162,10 @@ def read_bvals_bvecs(bvals_path, bvecs_path, min_bval=100.):
         raise ValueError("b-values and b-vectors shapes do not correspond.")
 
     # Infer nb of T2 and nb of shells.
-    nb_nodiff = np.sum(bvals == 0)  # nb of volumes where bvalue=0
-    bvals_set = set(bvals) - {0}    # set of non-zero bvalues
+    nb_nodiff = np.sum(bvals < 50)  # nb of volumes where bvalue<50
+    b0_set = set(bvals[bvals < 50])
+    bvals_set = set(bvals) - b0_set    # set of non-zero bvalues
+    bvals_set = set([int(round(bval, -2)) for bval in list(bvals_set)])
     nb_shells = len(bvals_set)
     if min(bvals_set) < min_bval:
         raise ValueError("Small b-values detected (<{0}) in '{1}'.".format(
